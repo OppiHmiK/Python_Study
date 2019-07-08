f8e5c99095fa0e5d9f63aa60cc5f0cf4b63fa379
KimHippo <45390469+OppiHmiK@users.noreply.github.com>
2019-07-08 11:39:29 +0900

    2019_Big_Data_Academy_07.05_Lecture

---
+++ 2019_Big_Data_Academy/Python/2019.07.05/1st_exam_07.05.py
@@ -0,0 +1,25 @@
+''' number = input('숫자를 입력하세요 : ')
+print(number) '''
+
+print("Life""is""too short")
+print("Life"+"is"+"too short")
+print("Life","is","too short\n")
+
+print(3/24)
+print("3/24 : ", 3/24, end = '\n')
+
+a = 3; b = 4
+print('3을 4로 나눈 나머지는 : ', a % b)
+print('3을 4로 나눈 몫은 : ', a // b)
+print('3을 4로 나눈 결과는 : ', a / b, end = '\n')
+
+
+print('몫은 %d, 나머지는 %d입니다.'%(14//3, 14%3))
+say = "\"Python is very easy.\" he says"
+print(say)
+
+'''
+Life is too short
+You need python
+'''
+
---
+++ 2019_Big_Data_Academy/Python/2019.07.05/2nd_exam_07.05.py
@@ -0,0 +1,19 @@
+head = 'Python'; tail = ' is fun!'
+print(head + tail)
+print(head*3)
+
+print("="*50)
+print("My Program")
+print("="*50)
+
+print(len(head))
+print(len(tail))
+
+a = 'Life is too short, You need Python.'
+print(a[5], a[-1], a[-3])
+print(a[:])
+
+b = a[0:4]
+print(b)
+
+print('{} 문자길이는 {} 입니다.'.format(head+tail, len(head+tail)))
